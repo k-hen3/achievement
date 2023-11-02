58ee9a4baecfbe5815dafe65eb86bf67651737f0
kentarochann <kentaro4413@gmail.com>
2023-11-02 22:51:18 +0900

    入力・表示・削除を実装

--- index.js
+++ index.js
@@ -1,134 +1,187 @@
-let formCount = 1;
-console.log(`現在のフォームの数：${formCount}`);
-let doughnutChart = null;
+// $(document).ready(function() {
 
-$(".add").click(function(){
-    ++formCount;
-    console.log("現在のフォームの数：" + formCount);
+    // 初期化
+    let formCount = 1;
+    // console.log(`現在のフォームカウント：${formCount}`);
+    let doughnutChart = null;
 
-    let newForm = $(".input_container1").prop('outerHTML');
-    newForm = newForm.replace(/1/g, formCount);
+    // フォーム初期化
+    let newForm = '<div class="input_container" id="input_container' + formCount + '">';
+    newForm += '<div class="text_form_container">';
+    newForm += '<label for="todo' + formCount + '">todo' + formCount + '：</label>';
+    newForm += '<input type="text" id="todo' + formCount + '" name="todo' + formCount + '" placeholder="例)５時間勉強する">';
+    newForm += '</div>';
+    newForm += '<div class="rate_form_container">';
+    newForm += '<label for="rate' + formCount + '">達成率：</label>';
+    newForm += '<input type="number" id="rate' + formCount + '" name="todo' + formCount + '" min="0" max="100" placeholder="例)50"> %';
+    newForm += '</div>';
+    newForm += '<div class="remove_form_container">';
+    newForm += '<button class="remove">削除</button>';
+    newForm += '</div>';
+    newForm += '</div>';    
     $(".input_section").append(newForm);
-});
 
-$(".watch").click(function(){
-    // console.log("pushed");
+    // フォーム追加
+    function addForm(){
+        ++formCount;
+        // console.log("現在のフォームカウント：" + formCount);
 
-    // 既存のものを一旦削除
-    $('.todo_name').empty();
-    $('.rate_result').empty();
-    $('.all_result').empty();
-    if(doughnutChart){
-        doughnutChart.destroy();
-    }
+        newForm = '<div class="input_container" id="input_container' + formCount + '">';
+        newForm += '<div class="text_form_container">';
+        newForm += '<label for="todo' + formCount + '">todo' + formCount + '：</label>';
+        newForm += '<input type="text" id="todo' + formCount + '" name="todo' + formCount + '" placeholder="例)５時間勉強する">';
+        newForm += '</div>';
+        newForm += '<div class="rate_form_container">';
+        newForm += '<label for="rate' + formCount + '">達成率：</label>';
+        newForm += '<input type="number" id="rate' + formCount + '" name="todo' + formCount + '" min="0" max="100" placeholder="例)50"> %';
+        newForm += '</div>';
+        newForm += '<div class="remove_form_container">';
+        newForm += '<button class="remove">削除</button>';
+        newForm += '</div>';
+        newForm += '</div>';
 
-    const inputContainer = $("[class^='input_container']");
-    const inputContainerCount = inputContainer.length;
-
-    let rates = [];
-    let todos = [];
-    let todoObj = {};
-
-    // １つずつフォームのhtml構造を取り出す
-    console.log('==============');
-    for (let i = 1; i <= inputContainerCount; i++) {
+        $(".input_section").append(newForm);
         
-        // console.log(i);
-        const todoId = `#todo${i}`;
-        const rateId = `#rate${i}`;
-        // console.log(todoId);
+    };
 
-        const todoVal = $(todoId).val();
-        const rateVal = $(rateId).val();
-        console.log('task number' + i + ' : ' + todoVal);
-        console.log('rate number' + i + ' : ' + rateVal);
-
-        // 画面出力
-        $('.individual_result').append(`<div class="todo_name">todo${i} : ${todoVal}</div>`);
-        $('.individual_result').append(`<div class="rate_result">rate : ${rateVal} %</div>`);
-
-        console.log('==============');
-
-        // それぞれの達成率をまとめる
-        rates.push(rateVal);
-        // それぞれのtodoをまとめる
-        todos.push(todoVal);
-        // オブジェクトにまとめる
-        todoObj[todoVal] = rateVal;
-    }
-
-    console.log(`todos：${todos}`);
-    console.log(todoObj);
-
-    // 全体の目標達成率
-    console.log(`全体の達成率、配列：${rates}`);
-    achievementRate = rates.reduce((achievementRate, rates) => achievementRate + parseInt(rates), 0);
-
-    achievementRate = achievementRate / formCount;
-    achievementRate = Math.round(achievementRate);
-    console.log(`全体の達成率：${achievementRate}`);
-
-    $('.all_result').append(`全体の達成率：${achievementRate} %`);
-    
-    // グラフ用の達成率を入れる配列を作成
-    let doughnutRates = [];
-    
-    rates.forEach(rate => {
-        // rates配列の中身を１つずつformCountで割る
-        let doughnutRate = rate / formCount;
-        doughnutRate = Math.round(doughnutRate);
-        console.log(doughnutRate);
-        // 新しい配列に入れる
-        doughnutRates.push(doughnutRate);
+    // 各フォームごとの削除
+    $(".input_section").on("click", ".remove", function() {
+        // --formCount;
+        $(this).closest($("[class^='input_container']")).remove();
     });
-    console.log(doughnutRates);
-    
-    // 未達率
-    const unachieveRate = 100 - achievementRate;
-    console.log(`未達率：${unachieveRate}`);
-    todos.push('未達率');
-    doughnutRates.push(unachieveRate);
 
-    // 円グラフの色を作成
-    let colorCodeArray = [];
-    console.log(`フォームカウント：${formCount}`);
-    for (let i = 0; i < formCount; i++) {
-        let randomColorCode;
-        // ランダムにカラーコードを生成。#a9a9a9になったらもう一度
-        do {
-            let colorCode = Math.floor(Math.random() * 16777215).toString(16);
-            for (let count = colorCode.length; count < 6; count++) {
-                colorCode = "0" + colorCode;
+    // 達成度表示
+    function displayResult(){
+        // console.log("pushed");
+
+        let formValid = true;
+        let todos = [];
+        let rates = [];
+
+        // バリデーションエラー
+        $(".input_container input").each(function() {
+            if ($(this).val() === "") {
+                formValid = false;
+                return false; // バリデーションエラー
             }
-            randomColorCode = "#" + colorCode;
-        } while (randomColorCode === "#a9a9a9");
-    
-        colorCodeArray.push(randomColorCode)
-    }
-    colorCodeArray.push('#a9a9a9');
-    console.log(`カラーコード:${colorCodeArray}`);
+        });
 
-    // グラフ作成
-    let context = $('.chart');
-    doughnutChart = new Chart(context, {
-        type: 'doughnut',
-        data: {
-            // labels: ["サーモン", "ハマチ", "マグロ", "エンガワ"],
-            labels: todos,
-            datasets: [{
-                // backgroundColor: ["#fa8072", "#00ff7f", "#00bfff", "#a9a9a9"],
-                backgroundColor: colorCodeArray,
-                // data: [60, 20, 15, 5]
-                data: doughnutRates
-            }]
-        },
-        options: {
-            responsive: false
+        // todoと達成率を配列にまとめる
+        if (formValid) {
+            // console.log('true');
+            $(".input_container input[type='text']").each(function() {
+                todos.push($(this).val());
+            });
+            
+            $(".input_container input[type='number']").each(function() {
+                rates.push($(this).val());
+            });
+            
+            $(".validationMessage").empty();
+        } else {
+            // console.log('false');
+            $(".all_result").empty();
+            $(".validationMessage").html("全て入力してください");
         }
+
+
+        // 既存のものを一旦削除
+        $('.all_result').empty();
+        if(doughnutChart){
+            doughnutChart.destroy();
+        }
+
+
+        // 全体の目標達成率
+        // console.log(`全体の達成率、配列：${rates}`);
+        achievementRate = rates.reduce((achievementRate, rates) => achievementRate + parseInt(rates), 0);
+
+        // console.log(`タスクの数：${todos.length}`);
+        achievementRate = achievementRate / todos.length;
+        // console.log(`フォームカウント：${formCount}`);
+        achievementRate = Math.round(achievementRate);
+        console.log(`全体の達成率：${achievementRate}`);
+
+        if(achievementRate){
+            $('.all_result').append(`全体の達成率：${achievementRate} %`);
+        } else {
+            $('.all_result').append("<div>すべて入力したら結果が見れます</div>");
+        }
+        
+        // グラフ用の達成率を入れる配列を作成
+        let doughnutRates = [];
+        
+        rates.forEach(rate => {
+            // rates配列の中身を１つずつformCountで割る
+            let doughnutRate = rate / todos.length;
+            doughnutRate = Math.round(doughnutRate);
+            console.log(doughnutRate);
+            // 新しい配列に入れる
+            doughnutRates.push(doughnutRate);
+        });
+        console.log(doughnutRates);
+        
+        // 未達率
+        const unachieveRate = 100 - achievementRate;
+        console.log(`未達率：${unachieveRate}`);
+        todos.push('未達率');
+        doughnutRates.push(unachieveRate);
+
+        // 円グラフの色を作成
+        let colorCodeArray = [];
+        for (let i = 0; i < (todos.length - 1); i++) {
+            let randomColorCode;
+            // ランダムにカラーコードを生成。#a9a9a9になったらもう一度
+            do {
+                let colorCode = Math.floor(Math.random() * 16777215).toString(16);
+                for (let count = colorCode.length; count < 6; count++) {
+                    colorCode = "0" + colorCode;
+                }
+                randomColorCode = "#" + colorCode;
+            } while (randomColorCode === "#a9a9a9");
+        
+            colorCodeArray.push(randomColorCode)
+        }
+        colorCodeArray.push('#a9a9a9');
+        console.log(`カラーコード:${colorCodeArray}`);
+
+        // グラフ作成
+        if (formValid) {
+            let context = $('.chart');
+            doughnutChart = new Chart(context, {
+                type: 'doughnut',
+                data: {
+                    // labels: ["サーモン", "ハマチ", "マグロ", "エンガワ"],
+                    labels: todos,
+                    datasets: [{
+                        // backgroundColor: ["#fa8072", "#00ff7f", "#00bfff", "#a9a9a9"],
+                        backgroundColor: colorCodeArray,
+                        // data: [60, 20, 15, 5]
+                        data: doughnutRates
+                    }]
+                },
+                options: {
+                    responsive: false
+                }
+            });    
+        }
+        
+        // // // 帯グラフで出力する
+        // // let chartBar = $('.chart_bar');
+    };
+    
+    // "フォームを追加"ボタンのクリックイベント
+    $(".add").click(function() {
+        addForm();
+    });
+
+    // "内容を表示"ボタンのクリックイベント
+    $(".resultButton").click(function() {
+        displayResult();
     });
     
-    // // 帯グラフで出力する
-    // let chartBar = $('.chart_bar');
+// });
+
 
 
     // 入力フォーム関係
@@ -139,4 +192,3 @@ $(".watch").click(function(){
     // グラフ関係
     // todo:グラフの見た目を山みたいにして、頂上を達成にする
     // todo:個々の達成率を帯グラフで％、CSSで
-});
